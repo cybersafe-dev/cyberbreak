2d76262c856b44116455d2ee68d0f316d86603ae
Jamie Carter <jc2820@hotmail.com>
2020-09-22 16:14:15 +0100

    add update with score function

---
+++ functions/addScoreToUser.js
@@ -0,0 +1,40 @@
+var Airtable = require("airtable");
+
+exports.handler = async (event) => {
+  const { uid, finalScore } = JSON.parse(event.body);
+  var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+    "applnhFkWhSAQIvg7"
+  );
+
+  base("users")
+    .update([
+      {
+        id: uid,
+        fields: {
+          score: finalScore,
+        },
+      },
+    ])
+    .then((records) => {
+      records.forEach(function (record) {
+        console.log("updated", record.get("name"));
+        return {
+          statusCode: 200,
+          body: "Successfully updated",
+          headers: {
+            "Access-Control-Allow-Methods": "*",
+          },
+        };
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      return {
+        statusCode: 400,
+        body: "Error updating with score",
+        headers: {
+          "Access-Control-Allow-Methods": "*",
+        },
+      };
+    });
+};
