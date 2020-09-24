d9d7bf1d7dbbe40c7b5b7f20c520c067f8945055
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-09-24 18:05:29 +0100

    Add ptsb logo and tweak styling

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LandingHeader from "./LandingHeader"
+import Ptsb from "../../images/blk-ptsb.svg"
 
 import "./Landing.css"
 
@@ -14,9 +15,10 @@ const Landing = () => (
       ever CyberSafeIreland Cyber Break, by disconnecting from devices and the
       online world to reconnect with your family for a whole 24 hours!
     </p>
-
+    <img src={Ptsb} alt="" className="ptsb-logo"/>
   </main>
   <Link to="/form" className="btn">Lets Go!</Link>
+  <img src={Ptsb} alt="" className="ptsb-logo"/>
   </>
 );
 
