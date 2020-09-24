8a9b142536c367b6282f0feab55c16584b5c3083
Jamie Carter <jc2820@hotmail.com>
2020-09-24 19:54:35 +0100

    style 404 page

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Error.css";
+import Lamp from "../../assets/images/lamp.svg"
 
-const Error = () => <h1>404: Page not found</h1>;
+const Error = () => (
+  <main className="error-container">
+    <h1 className="error-heading">404: Page not found</h1>
+    <Link to="/" className="back-start">Back to the start</Link>
+    <img src={Lamp} alt="lamp" className="lamp" />
+  </main>
+);
 
 export default Error;
