9509c999aabac85553e7723898660bed1e11b4e5
Jamie Carter <jc2820@hotmail.com>
2020-09-30 20:18:27 +0100

    remove android sock hop and reorder tunes

--- src/utils/music.js
+++ src/utils/music.js
@@ -1,4 +1,3 @@
-import AndroidSockHop from "../assets/sounds/Android Sock Hop.mp3";
 import KingsOfTara from "../assets/sounds/Kings of Tara.mp3";
 import OnionCapers from "../assets/sounds/Onion Capers.mp3";
 import Robobozo from "../assets/sounds/Robobozo.mp3";
@@ -7,22 +6,20 @@ import Zazie from "../assets/sounds/Zazie.mp3";
 export const quizMusic = (num) => {
   let currentMusic;
   switch (num) {
-    case 1:
+    case 2:
     case 6:
+    case 9:
       currentMusic = OnionCapers;
       break;
-    case 2:
+    case 3:
     case 7:
       currentMusic = Robobozo;
       break;
-    case 3:
+    case 4:
     case 8:
       currentMusic = Zazie;
       break;
-    case 4:
-    case 9:
-      currentMusic = AndroidSockHop;
-      break;
+    case 1:
     case 5:
     case 10:
       currentMusic = KingsOfTara;
