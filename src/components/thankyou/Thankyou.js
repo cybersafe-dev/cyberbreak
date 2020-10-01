ecc7fe642d72292b631fe403b38b5c55277bf778
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-10-01 18:29:52 +0200

    Add green logo and tweak responsive things on score page

--- src/components/thankyou/Thankyou.js
+++ src/components/thankyou/Thankyou.js
@@ -7,7 +7,7 @@ import MusicModal from "../musicModal/MusicModal";
 import Note from "../../assets/images/note.svg";
 import Lock from "../../assets/images/white-lock.svg";
 import Ptsb from "../../assets/images/wyt-ptsb.png";
-import Csi from "../../assets/images/csi-logo.png";
+import Csi from "../../assets/images/green-csi-logo.png";
 import "./Thankyou.css";
 
 const Thankyou = () => {
@@ -121,7 +121,7 @@ const Thankyou = () => {
         <section className="options">
           <div className="logos-thankyou">
             <img src={Ptsb} alt="" />
-            <img src={Csi} alt="" />
+            <img src={Csi} alt="" className="c-logo" />
           </div>
           <button
             type="submit"
