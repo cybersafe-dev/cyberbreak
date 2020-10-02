0e4d6263d226b0e7421c86f8885d1caa4f6ce599
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-10-02 17:19:45 +0200

    Remove clouds for faff and add env files

--- src/components/survey/Survey.js
+++ src/components/survey/Survey.js
@@ -6,7 +6,6 @@ import { surveyBackgroundColor } from "../../utils/surveyBackground";
 import { surveyLogoColor } from "../../utils/surveyLogos";
 import Cyclist from "../../assets/images/cyclist-svg-less-trees.svg";
 import Ptsb from "../../assets/images/wyt-ptsb.png";
-import Clouds from "../../assets/images/cloud-logo.svg";
 import Kite from "../../assets/images/kite.svg";
 import "./Survey.css";
 
@@ -68,7 +67,6 @@ console.log(currentLogo)
         className="question-container"
         style={{ backgroundColor: currentBg }}
       >
-        <img src={Clouds} alt="clouds" className="clouds" />
         <img src={Kite} alt="flying kite" className="loading-kite" />
       </main>
     );
